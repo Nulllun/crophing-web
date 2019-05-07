d69317baa29c0f9abe8172ebe04fd0962c75f93e
Michael Lun <michaeld23046e123@gmail.com>
2019-05-07 23:33:52 +0800

    add rotate function, refine code

--- app/assemblePhoto.js
+++ app/assemblePhoto.js
@@ -1,85 +1,6 @@
 var width = window.innerWidth;
 var height = window.innerHeight;
-var anchorList = [];
-
-function update(activeAnchor) {
-    var group = activeAnchor.getParent();
-    var topLeft = group.find('.topLeft')[0];
-    var topRight = group.find('.topRight')[0];
-    var bottomRight = group.find('.bottomRight')[0];
-    var bottomLeft = group.find('.bottomLeft')[0];
-    var image = group.find('Image')[0];
-    var anchorX = activeAnchor.getX();
-    var anchorY = activeAnchor.getY();
-    // update anchor positions
-    switch (activeAnchor.getName()) {
-        case 'topLeft':
-            topRight.y(anchorY);
-            bottomLeft.x(anchorX);
-            break;
-        case 'topRight':
-            topLeft.y(anchorY);
-            bottomRight.x(anchorX);
-            break;
-        case 'bottomRight':
-            bottomLeft.y(anchorY);
-            topRight.x(anchorX);
-            break;
-        case 'bottomLeft':
-            bottomRight.y(anchorY);
-            topLeft.x(anchorX);
-            break;
-    }
-    image.position(topLeft.position());
-    var width = topRight.getX() - topLeft.getX();
-    var height = bottomLeft.getY() - topLeft.getY();
-    if (width && height) {
-        image.width(width);
-        image.height(height);
-    }
-}
-function addAnchor(group, x, y, name) {
-    var stage = group.getStage();
-    var layer = group.getLayer();
-    var anchor = new Konva.Circle({
-        x: x,
-        y: y,
-        stroke: '#666',
-        fill: '#ddd',
-        strokeWidth: 2,
-        radius: 8,
-        name: name,
-        draggable: true,
-        dragOnTop: false
-    });
-    anchor.on('dragmove', function () {
-        update(this);
-        layer.draw();
-    });
-    anchor.on('mousedown touchstart', function () {
-        group.draggable(false);
-        this.moveToTop();
-    });
-    anchor.on('dragend', function () {
-        group.draggable(true);
-        layer.draw();
-    });
-    // add hover styling
-    anchor.on('mouseover', function () {
-        var layer = this.getLayer();
-        document.body.style.cursor = 'pointer';
-        this.strokeWidth(4);
-        layer.draw();
-    });
-    anchor.on('mouseout', function () {
-        var layer = this.getLayer();
-        document.body.style.cursor = 'default';
-        this.strokeWidth(2);
-        layer.draw();
-    });
-    group.add(anchor);
-    anchorList.push(anchor);
-}
+var rotateFrameList = [];
 
 var stage = new Konva.Stage({
     container: 'model_preview',
@@ -95,28 +16,30 @@ function importParts(imgSrc) {
     imageObj.onload = function () {
         var newImg = new Konva.Image({
             width: imageObj.width,
-            height: imageObj.height
-        });
-
-        var newImgGroup = new Konva.Group({
-            x: 180,
-            y: 50,
+            height: imageObj.height,
+            x: 80,
+            y: 80,
             draggable: true
         });
 
-        layer.add(newImgGroup);
-        newImgGroup.add(newImg);
-        addAnchor(newImgGroup, 0, 0, 'topLeft');
-        addAnchor(newImgGroup, imageObj.width+1, 0, 'topRight');
-        addAnchor(newImgGroup, imageObj.width+1, imageObj.height+1, 'bottomRight');
-        addAnchor(newImgGroup, 0, imageObj.height+1, 'bottomLeft');
+        layer.add(newImg);
         newImg.image(imageObj);
-        newImgGroup.on('dragstart', function() {
+        var rotateFrame = new Konva.Transformer({
+            node: newImg,
+            centeredScaling: true,
+            rotationSnaps: [0, 90, 180, 270],
+            resizeEnabled: true
+        });
+        rotateFrameList.push(rotateFrame);
+        layer.add(rotateFrame);
+        newImg.on('dragstart', function() {
             this.moveToTop();
+            rotateFrame.moveToTop();
             layer.draw();
         });
-        newImgGroup.on('dblclick dbltap', function() {
+        newImg.on('dblclick dbltap', function() {
             this.destroy();
+            rotateFrame.destroy();
             layer.draw();
           });
         layer.draw();
@@ -166,15 +89,15 @@ function savePhoto() {
 }
 
 function hide(){
-    for (var key in anchorList){
-        anchorList[key].hide()
+    for (var key in rotateFrameList){
+        rotateFrameList[key].hide()
     }
     layer.draw();
 }
 
 function show(){
-    for (var key in anchorList){
-        anchorList[key].show()
+    for (var key in rotateFrameList){
+        rotateFrameList[key].show()
     }
     layer.draw();
 }
